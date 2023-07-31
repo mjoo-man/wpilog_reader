b1923ea2c8b96e160eca8d5eeacfa61f19b65241
Micah <mjooevermann@gmail.com>
2023-07-30 19:04:48 -0500

    added functionality to fill NaN in files

--- fileOps.py
+++ fileOps.py
@@ -2,6 +2,8 @@
 from tkinter import filedialog as fd
 from tkinter import Tk
 
+import os
+
 # function will prompt the user to select a directory and return its absolute path
 def getFilePath(message):
 	Tk().withdraw()
@@ -9,14 +11,12 @@ def getFilePath(message):
 	return filepath
 
 # filter out non csv files
-import os
-
 def getCSVFiles(directory):
-    files = os.listdir(directory)
-    filenames = []
-    for i in range(len(files)):
-        # read only .csv files
-        filename, extension =  os.path.splitext(files[i])
-        if extension == '.csv': 
-            filenames.append(filename+extension)
-    return filenames
+	files = os.listdir(directory) # list all files in directory
+	filenames = []
+	for i in range(len(files)):
+		# read only .csv files
+		filename, extension =  os.path.splitext(files[i])
+		if extension == '.csv': 
+			filenames.append(filename+extension)
+	return filenames # return only csv paths
