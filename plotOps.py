8febfc9285c2b67c7f597d2aa7bbb84efdf2d97f
mjoevermannTAMU <mjooevermann@tamu.edu>
2023-08-10 14:08:25 -0500

    plot drive and steer separately

--- plotOps.py
+++ plotOps.py
@@ -4,7 +4,18 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import sys 
 
+def getHeaderstoPlot(simple_headers, des_headers):
+    idx_of_des_headers = []
+    for h in des_headers:
+        try:
+            idx_of_des_headers.append(simple_headers.index(h))
+
+        except ValueError as e:
+            print("ERROR: ", e)
+            continue
+    return idx_of_des_headers
 
 def findEnableDisable(data):
     cycles = []
@@ -48,46 +59,42 @@ def plotWPILog():
         headers = pd.read_csv(file, index_col=0, nrows=0).columns.tolist()
 
         simple_headers = [headers[i].replace("NT:/SmartDashboard/", "") for i in range(len(headers))]
-        
-        simple_headers.index('Ball Pressure (psi)')
-        #steering params
-        steering = []
-        steering.append(simple_headers.index('Pipe Angle'))
-        steering.append(simple_headers.index('Pend Angle'))
-        # steering.append(simple_headers.index('Ball Pressure (psi)'))
-        steering.append(simple_headers.index('Commanded Steer Position'))
-                
-        # steering.append(simple_headers.index('NT:/FMSInfo/FMSControlData'))
-    
+               
+        des_headers_drive = ['Drive Angle', 'Commanded Drive Velocity', 'Drive Velocity']
+        des_headers_pipe = ['Pipe Angle', 'Pend Angle', 'Commanded Steer Position']
+
         # read data in the enabled disabled
         run_data = pd.read_csv(file, index_col=0) # timestamp is the index
         # TODO: parse for data i want and plot them together
-        
         index_enable = findEnableDisable(run_data)
         numPlots = getNumPlots(index_enable)
         press_index = simple_headers.index('Ball Pressure (psi)')
 
-        # plot desired curves
-        for i in range(numPlots):
-            plt.figure(i+1)
-            for p in steering: # for each data stream
+        for d in [des_headers_drive, des_headers_pipe]:
+            idx_plot_headrs = getHeaderstoPlot(simple_headers, d)
+
+            # plot desired curves
+            for i in range(numPlots):
+                plt.figure()
+                for p in idx_plot_headrs: # for each data stream
+                    try:
+                        run_data[headers[p]][index_enable[2*i]:index_enable[2*i+1]].plot(label=simple_headers[p])
+                    except IndexError:
+                        # if index happens to be out of range, plot the remainder of the file
+                        run_data[headers[p]][index_enable[2*i]::].plot(label=simple_headers[p])
                 try:
-                    run_data[headers[p]][index_enable[2*i]:index_enable[2*i+1]].plot(label=simple_headers[p])
+                    runPressure = round(run_data[headers[press_index]][index_enable[2*i]:index_enable[2*i+1]].mean(), 2)
                 except IndexError:
-                    # if index happens to be out of range, plot the remainder of the file
-                    run_data[headers[p]][index_enable[2*i]::].plot(label=simple_headers[p])
-            try:
-                runPressure = round(run_data[headers[press_index]][index_enable[2*i]:index_enable[2*i+1]].mean(), 2)
-            except IndexError:
-                runPressure = round(run_data[headers[press_index]][index_enable[2*i]::].mean(), 2)
-            plt.title(f"Response at {runPressure} psi")
-            plt.grid()
-            plt.legend()
+                    runPressure = round(run_data[headers[press_index]][index_enable[2*i]::].mean(), 2)
+                plt.title(f"Response at {runPressure} psi")
+                plt.grid()
+                plt.legend()
         
         # old code for checking
-        # plt.figure(99)
-        # for p in headers:
-        #     run_data[p].plot(label=p.replace("NT:/SmartDashboard/", ""))
+        plt.figure(99)
+        for p in headers:
+            run_data[p].plot(label=p.replace("NT:/SmartDashboard/", ""))
+        plt.legend()
         plt.show()
         # TODO: crop the data to desirable ranges, plot and save it to combine
         # with the theoretical results
