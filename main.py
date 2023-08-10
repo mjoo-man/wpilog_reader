2b7a3a0de6da3075fe44961f3911e66f909c0239
mjoevermannTAMU <mjooevermann@tamu.edu>
2023-08-10 14:07:44 -0500

    disable all files

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def main():
     ''' uncomment to preprocess the wpilog files '''
     # fillWPIlog()
 
-    ''' if logs are already preprocessed plot them for each log and each enable diable cycle'''
+    ''' if logs are already preprocessed plot them for each log and each enable disable cycle'''
     # plotWPILog()
 
     '''A custom plot to stack pipe responses on pressures'''
     #function defaults to plotting, but if called stackPressures(True) itll return a dictionary with the time values and data
-    stackPressures()
+    # stackPressures()
 
 main()
